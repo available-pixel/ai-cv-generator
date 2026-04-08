6d14e483b9b639cc1fff4b309bc19a9bbade12d7
Fadil ADELABOU <fadiladelabou@gmail.com>
2026-04-08 21:54:11 +0100

    fix: use st.pdf for preview

--- app.py
+++ app.py
@@ -323,18 +323,8 @@ elif menu == "📄 Preview & Download":
             "application/pdf"
         )
 
-        # ✅ Browser preview using EMBED (works in Chrome)
-        import base64
-        base64_pdf = base64.b64encode(pdf_bytes).decode("utf-8")
-
-        pdf_display = f"""
-        <embed 
-            src="data:application/pdf;base64,{base64_pdf}" 
-            width="100%" 
-            height="700px" 
-            type="application/pdf">
-        """
-
-        st.markdown(pdf_display, unsafe_allow_html=True)
+        # ✅ Preview
+        st.write("### 📄 Preview")
+        st.pdf(pdf_bytes)
 
     st.markdown("</div>", unsafe_allow_html=True)
